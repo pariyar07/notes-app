1b4974bf72d328aacfcf95aa18cf74fd422c1f8e
Satyam Pariyar <pariyarsatyam1@gmail.com>
2021-12-11 22:49:30 +0530

    Addes Local Storage feature

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const addNote = document.querySelector('#add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes){
+    notes.forEach(note => {
+        addNewNote(note);
+    })
+}
+
 addNote.addEventListener('click', () => {
     addNewNote();
 })
 
-function addNewNote() {
+function addNewNote(text = "") {
     const note = document.createElement('div');
     note.classList.add('note');
     document.body.appendChild(note);
@@ -14,11 +22,10 @@ function addNewNote() {
                 <button id="edit"><i class="fa fa-edit"></i></button>
                 <button id="delete"><i class="fa fa-trash"></i></button>
             </div>
-            <textarea></textarea>
-            <div class="main hidden"></div>
+            <div class="main ${text ? '' : 'hidden'}"></div>
+            <textarea class="${text ? 'hidden' : ''}"></textarea>
         </div>
     `;
-    
 
     const editBtn = note.querySelector('#edit');
     const deleteBtn = note.querySelector('#delete');
@@ -29,17 +36,30 @@ function addNewNote() {
     editBtn.addEventListener('click', () => {
         mainText.classList.toggle('hidden');
         textArea.classList.toggle('hidden');
+        updateLS();
     })
+
+    textArea.value = text;
+    mainText.innerHTML = text;
     
     textArea.addEventListener("input", () => {
         let input = textArea.value;
         mainText.innerHTML = input;
+        updateLS();
     })
     
     deleteBtn.addEventListener('click', () => {
         note.remove();
+        updateLS();
     })
-    
 }
 
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+    const notes = [];
+    notesText.forEach(note => {
+        notes.push(note.value);
+    })
 
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
